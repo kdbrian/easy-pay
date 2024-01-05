90740b8203ed5a319f1a2ce997f22de00adf4f53
nullpointerexpected <juniorprogrammer09@gmail.com>
2024-01-05 08:47:13 +0300

    changes

--- src/main/java/io/github/junrdev/easypay/util/SecurityConfig.java
+++ src/main/java/io/github/junrdev/easypay/util/SecurityConfig.java
@@ -2,20 +2,80 @@ package io.github.junrdev.easypay.util;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.AbstractRequestMatcherRegistry;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetails(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder()
+                .username("ADMIN")
+                .password(passwordEncoder.encode("h3ll0w0rld"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
-        httpSecurity.csrf().disable().authorizeHttpRequests(AbstractRequestMatcherRegistry::anyRequest);
+        return httpSecurity.csrf()
+                .disable()
 
-        return httpSecurity.build();
+                .authorizeHttpRequests()
+                .requestMatchers("/")
+                .permitAll()
+                .and()
+
+                .authorizeHttpRequests()
+
+                // must be logged in to access any route
+                .requestMatchers("/payments").authenticated()
+
+                .and()
+                .formLogin()
+                .and()
+
+                // logout
+                .logout()
+                .logoutSuccessUrl("/login")
+
+                .and()
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetails(passwordEncoder()));
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
